ebb09ce00952bb943c5ed8bae1e01a1f40ef01d8
PRAVEEN200307;a <crouselearn@gmail.com>
2023-12-16 12:39:48 +0530

    remove meathods

--- DOM/index.js
+++ DOM/index.js
@@ -5,21 +5,16 @@ const btn =document.getElementById('input-btn');
 const userInput =document.getElementById('user-input');
 
 let foodContaiener =document.getElementById('food-container');
-console.log(foodContaiener);
-btn.addEventListener('click',()=>{
-   const li =document.createElement('li');
-   li.textContent =userInput.value;
-   li.className="list-items"
-   foodContaiener.append(li);
-});
-//Clone
+// console.log(foodContaiener);
+// btn.addEventListener('click',()=>{
+//    const li =document.createElement('li');
+//    li.textContent =userInput.value;
+//    li.className="list-items"
+//    foodContaiener.append(li);
+// });
 
-const  AsyncBtn =document.getElementById('AsyncBtn');
-
-AsyncBtn.addEventListener('click',()=>{
-     
-    let cloneDiv =document.getElementById('duplicate');
-    cloneDiv.innerHTML='';  
-    const cloneTrueEL =foodContaiener.cloneNode(true);
-    cloneDiv.append(cloneTrueEL);
-});
+//remove
+function removeitem(event){
+  const removeLi =event.target.parentNode.parentNode;
+  removeLi.remove();  
+}
