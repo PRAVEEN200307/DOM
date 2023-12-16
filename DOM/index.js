bb1fec6e8853fb5dace18400f907db5ff24b2d11
PRAVEEN200307;a <crouselearn@gmail.com>
2023-12-16 07:17:27 +0530

    inserAdjacent;

--- DOM/index.js
+++ DOM/index.js
@@ -1,27 +1,32 @@
+/*
+button
 
+const btn =document.getElementById('input-btn');
+const userInput =document.getElementById('user-input');
+*/
 
-//button
-// const btn =document.getElementById('input-btn');
-//user -input
-// const userInput =document.getElementById('user-input');
+let foodContaiener =document.getElementById('food-container')
 
+/**
+ * beforebegin
+ * <ul>
+ *   afterbegin
+ * 
+ *  beforeend 
+ * </ul>
+ * afterend
+ */
 
+// foodContaiener.insertAdjacentHTML("afterbegin","<h1> I am here </h1>");
+// foodContaiener.insertAdjacentHTML("afterend","I am here");
+// foodContaiener.insertAdjacentHTML("beforebegin","I am here");
+// foodContaiener.insertAdjacentHTML("beforeend","I am here");
+                                                                         
 
-let foodContaiener =document.getElementById('food-container');
+// foodContaiener.insertAdjacentText("beforeend","<h1>I am here</h1>");
 
-const createliEL =document.createElement('li');
-createliEL.className="list-items"
-createliEL.textContent=`Maagi`;
-
-foodContaiener.append(createliEL); //append
-foodContaiener.prepend(createliEL); //prepend
-
-foodContaiener.before(createliEL); //before
-foodContaiener.after(createliEL); //after
-
-
-//Old way of doing
-// foodContaiener.parentNode.insertBefore(createliEL,foodContaiener);//old way--before
-foodContaiener.parentNode.insertBefore(createliEL,foodContaiener);//old way--before
-foodContaiener.parentNode.insertBefore(createliEL,foodContaiener.nextSibling);//old way--before
+// const p = document.createElement('li');
+// p.textContent="biriyani";
+// p.className="list-items"
+// foodContaiener.insertAdjacentElement("beforeend",p);
 
