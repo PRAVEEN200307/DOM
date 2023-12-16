9d0c0d88e2061a5c590631775d7d47614a37e995
PRAVEEN200307;a <crouselearn@gmail.com>
2023-12-16 11:38:07 +0530

    Cloned nodes

--- DOM/index.js
+++ DOM/index.js
@@ -1,37 +1,25 @@
 /*
 button
-
+*/
 const btn =document.getElementById('input-btn');
 const userInput =document.getElementById('user-input');
-*/
 
-
-
-//Replace Dom elememts
-  //replaceWith
-  //old way
-    //  replaceChild
-    //  replaceChildren
 let foodContaiener =document.getElementById('food-container');
+console.log(foodContaiener);
+btn.addEventListener('click',()=>{
+   const li =document.createElement('li');
+   li.textContent =userInput.value;
+   li.className="list-items"
+   foodContaiener.append(li);
+});
+//Clone
 
-let samberLi =document.querySelector('#food-container :first-child');
-// console.log(samberLi);
+const  AsyncBtn =document.getElementById('AsyncBtn');
 
-const li =document.createElement('li');
-li.textContent ="Paruppu samber";
-li.className ="list-items";
-
-// samberLi.replaceWith(li); 
-
-//old way
-  //samberLi.parentNode.replaceChild(li,samberLi);//old way
-  
-const li1 = document.createElement("li");
-li1.textContent="mulaki sambar";
-li1.className="list-items";
-
-const li2 = document.createElement("li");
-li2.textContent="carrot rice";
-li2.className="list-items";
-
-foodContaiener.replaceChildren(li1,li2);
+AsyncBtn.addEventListener('click',()=>{
+     
+    let cloneDiv =document.getElementById('duplicate');
+    cloneDiv.innerHTML='';  
+    const cloneTrueEL =foodContaiener.cloneNode(true);
+    cloneDiv.append(cloneTrueEL);
+});
