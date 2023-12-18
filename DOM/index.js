ba1514736fac8a77158cd0f91e6e948d6c6d98ca
PRAVEEN200307;a <crouselearn@gmail.com>
2023-12-18 20:02:25 +0530

    git iniline style;

--- DOM/index.js
+++ DOM/index.js
@@ -9,21 +9,54 @@ button
 // console.log(li.closest('main'));
 
 const userInput =document.querySelector("#input-food-container input");
-console.dir(userInput);
 
+/*
+style="background-color: royalblue;border: none;
+padding: 2px 15px;color: #fff;border-radius: 2px;cursor: pointer;"
+*/
 
-//properties
-  //this only change a properites it don't change renter html
-  //this is a bad pratice
-// console.log("type :",userInput.type);
-// console.log('id :',userInput.id);
-// console.log("name :",userInput.name);
-// console.log("placeholder :",userInput.placeholder);
-// console.log(userInput.value);
-// userInput.value="praveen"
-// console.log(userInput.value);
+/*
+//1.setAttribute
+  const btnEl =document.querySelector("#input-food-container button");
+  // btnEl.setAttribute("style","background-color: royalblue;border: none;
+  padding: 2px 15px;color: #fff;border-radius: 2px;cursor: pointer;");           
 
-//Atribute
-const firstName=prompt("enter you name");
-userInput.setAttribute('value',`${firstName}`);
+//2.Direct Property access
+  console.log(btnEl.style);
 
+  btnEl.style.padding="2px 15px";
+  btnEl.style.backgroundColor="royalblue";
+  btnEl.style.border="none";
+  btnEl.style.color="#fff";
+  btnEl.style.borderRadius="2px";
+  btnEl.style.cursor="pointer";
+
+  12:32
+
+const btnEl =document.querySelector("#input-food-container button");
+//3.cssText
+btnEl.style.cssText="background-color: royalblue;border: none;padding: 2px 15px;color: #fff;border-radius: 2px;cursor: pointer;"
+
+btnEl.style.cssText +="background-color:red;"
+
+console.log(btnEl.style.cssText)
+
+//Getting the style
+console.log(btnEl.style.padding);
+console.log(btnEl.style.border);
+console.log(btnEl.style.backgroundColor);
+
+//getComputedStyle() meathod
+  //window meathod
+
+const overStateinput=document.querySelector('.input-parent button');
+console.log(overStateinput);
+
+console.log(window.getComputedStyle(overStateinput,":hover"));
+
+const btnEl =document.querySelector("#input-food-container button");
+btnEl.removeAttribute('style');
+
+btnEl.style.cssText="background-color: royalblue;border: none;padding: 2px 15px;color: #fff;border-radius: 2px;cursor: pointer;"
+
+*/
