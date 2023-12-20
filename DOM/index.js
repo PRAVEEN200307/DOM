ea6a4f00a2fddad45ed59ac821067aff59211a9a
PRAVEEN200307;a <crouselearn@gmail.com>
2023-12-20 07:46:20 +0530

    DOM animation

--- DOM/index.js
+++ DOM/index.js
@@ -1,62 +1,36 @@
-/*
-button
-// */
-// const li=document.querySelector('#food-container li');
-
-// console.log(li.parentNode);
-// console.log(li.parentElement.parentElement);
-
-// console.log(li.closest('main'));
 
 const userInput =document.querySelector("#input-food-container input");
 
-/*
-style="background-color: royalblue;border: none;
-padding: 2px 15px;color: #fff;border-radius: 2px;cursor: pointer;"
-*/
+const inputDiv =document.querySelector('#input-food-container');
+const hideBtn =document.querySelector('#hide-btn');
+// console.log(hideBtn);
 
-/*
-//1.setAttribute
-  const btnEl =document.querySelector("#input-food-container button");
-  // btnEl.setAttribute("style","background-color: royalblue;border: none;
-  padding: 2px 15px;color: #fff;border-radius: 2px;cursor: pointer;");           
+//task: show div based on click (toggle)
 
-//2.Direct Property access
-  console.log(btnEl.style);
+// hideBtn.addEventListener('click',()=>{
+//     if( inputDiv.style.display ==="none"){
+//        hideBtn.style.backgroundColor="gray";
+//        hideBtn.innerHTML="hide div";
+//        inputDiv.style.display="block";
+//     }
+//     else{
+//         inputDiv.style.display ="none";
+//         hideBtn.style.backgroundColor="royalblue";
+//         hideBtn.innerHTML="show text";
+//     }
+// });
 
-  btnEl.style.padding="2px 15px";
-  btnEl.style.backgroundColor="royalblue";
-  btnEl.style.border="none";
-  btnEl.style.color="#fff";
-  btnEl.style.borderRadius="2px";
-  btnEl.style.cursor="pointer";
 
-  12:32
-
-const btnEl =document.querySelector("#input-food-container button");
-//3.cssText
-btnEl.style.cssText="background-color: royalblue;border: none;padding: 2px 15px;color: #fff;border-radius: 2px;cursor: pointer;"
-
-btnEl.style.cssText +="background-color:red;"
-
-console.log(btnEl.style.cssText)
-
-//Getting the style
-console.log(btnEl.style.padding);
-console.log(btnEl.style.border);
-console.log(btnEl.style.backgroundColor);
-
-//getComputedStyle() meathod
-  //window meathod
-
-const overStateinput=document.querySelector('.input-parent button');
-console.log(overStateinput);
-
-console.log(window.getComputedStyle(overStateinput,":hover"));
-
-const btnEl =document.querySelector("#input-food-container button");
-btnEl.removeAttribute('style');
-
-btnEl.style.cssText="background-color: royalblue;border: none;padding: 2px 15px;color: #fff;border-radius: 2px;cursor: pointer;"
-
-*/
+//animation
+   hideBtn.addEventListener('click',()=>{
+      if(inputDiv.classList.contains('hide')){
+         inputDiv.classList.remove('hide');
+         hideBtn.innerHTML="hide div";
+         hideBtn.style.backgroundColor="gray";
+      }
+      else{
+         inputDiv.classList.add('hide');
+         hideBtn.style.backgroundColor="royalblue";
+         hideBtn.innerHTML="show text";
+      }
+   });
