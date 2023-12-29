95ec9c4fe8dd2995290d1278426f9658f479d6b5
PRAVEEN200307;a <crouselearn@gmail.com>
2023-12-29 11:20:41 +0530

    handlign input events

--- DOM/index.js
+++ DOM/index.js
@@ -1,81 +1,51 @@
-//formating
-//const formEl =document.forms.feedback;
+//forms input handle
 
+const formsEl =document.forms.feedback;
+let nameEL=formsEl.elements.fullname;
+
+
+console.log(nameEL);
 
 /*
-const handleSubmit=(event)=>{
-  event.preventDefault();
+  //!.Atributes
+  nameEL.value="praveen";
+  //nameEL.disabled='true';
+  nameEL.readOnly='true';
+  console.dir(nameEL.min);
 
-  const formdata=new FormData(formEl);
-  const data =[...formdata.entries()]
- 
- 
-  const JsonData =JSON.stringify(Object.fromEntries(formdata));
+  //Events
+  //foucs
+    nameEL.addEventListener('focus',()=>console.log('focused'));
+  //blur
+    nameEL.addEventListener('blur',()=>console.log("blur"));
+  //input
+    nameEL.addEventListener('input',(event)=>console.log(`${event.target.value}`));
+  //change
+   nameEL.addEventListener('change',(e)=>console.log(`changed value ${e.target.value}`));
+ */
 
-   //send to Backend
-    //XMLHttpRequest(XHR)
-        // let xhr = new XMLHttpRequest();
-        // xhr.open("GET","https://reqres.in/api/users/2",true);
-        // xhr.onload = function(){
-        
-        //   const obj= JSON.parse(xhr.responseText);
+  //meathods
+    nameEL.focus();
 
-        //   document.querySelector('#response').innerText=obj.data.first_name;
-        // }
-        // xhr.send();
-
-    //2.fetch()
-      //async
-    fetch("https://reqres.in/api/users?page=2",{
-       method:"GET",
-        // headers:{
-        //   "Content-Type":"application/json", //MIME- multi purpose internet mail extension
-        //   // "Content-Type":"application/x-www-form-urlencoded",  //when form queryselector
-        // },
-        // body :JsonData, //get a  object
-        // // body: new FormData(formEl),  //get the object string formate
+  //other events
+    nameEL.addEventListener('cut',()=>{
+      console.log('cuted')
+    });
+    nameEL.addEventListener('copy',()=>{
+       alert("this text can'ot copy ")
     })
-    .then(res => res.json())
-    .then( data =>{
-      document.querySelector('#response').innerText=JSON.stringify(data.data);
-    }) 
-
-}
-*/
-const formEl=document.forms.feedback
-
-const handleSubmit =(event)=>{
-   event.preventDefault();
-
-    const formdata=new FormData(formEl);
-    const formdataOb =JSON.stringify(Object.fromEntries(formdata));
-  //XMLHttpRequest
-  const xhr =new XMLHttpRequest();
-  xhr.open("GET","https://reqres.in/api/users/2",true);
-  xhr.onload=function(){
-      const obj =JSON.parse(xhr.responseText);
-      document.querySelector('#response').innerText = obj.data.first_name
-  }
-  xhr.send();//old way
-
-
-
- //fetch, axis
-  //post meathod have body
-    fetch('https://reqres.in/api/users/2',{
-      method:"POST",
-      headers:{
-        'Content-Type':'application/json'
-      },
-      body:formdataOb
+    nameEL.addEventListener("paste", (event) => {
+      console.log("paste")
     });
  
-    //GET MEATHOD
-    fetch('https://reqres.in/api/users?page=2',{
-      method:"GET",
-    })
-    .then(res => res.json())
-    .then(data => document.querySelector("#response").innerText=JSON.stringify(data.data));
 
-}
-formEl.addEventListener("submit",handleSubmit);
+handleSubmit =(event)=>{
+  event.preventDefault();
+ 
+  
+  const formdata =new FormData(formsEl);
+ 
+};
+
+formsEl.addEventListener("submit",handleSubmit);
+//1:58
