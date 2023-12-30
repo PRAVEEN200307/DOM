d2d7d769505bf25f5134cdb041ac38ffbc0895c7
PRAVEEN200307;a <crouselearn@gmail.com>
2023-12-30 18:51:49 +0530

    food cook planer app

--- DOM/index.js
+++ DOM/index.js
@@ -1,32 +1,49 @@
-
-//forms input handle
-
-const formsEl =document.forms.feedback;
-const hobbiesEl =formsEl.elements.hobbies;
-
-
-const terms =formsEl.elements.terms
 /*
-//Get properties & Methods
-  //console.log(hobbiesEl[0].value)
-
-//attribute
-  //hobbiesEl[0].checked =true;
-  //terms.checked=true;
-  // terms.indeterminate=true;
-  console.log(terms.value);
-//event
-  terms.addEventListener('change',(event)=>{
-      console.log(event.target.checked);
-  })
-//meathod
-terms.select();
-terms.checked = true;
+button
 */
+const btn =document.getElementById('input-btn');
+const userInput =document.getElementById('user-input');
+let foodContaiener =document.getElementById('food-container');
 
-//Task
-terms.addEventListener('change',(event)=>{
-    if(event.target.checked === true){
-      alert("I accept terms and conditions")
-    }
+const handleInuptFood =()=>{
+   const li =document.createElement('li');
+   li.className="list-items";
+   
+   const div =document.createElement('div');
+
+   if(userInput.value.length<1){
+       return 
+   }
+   else{
+    div.textContent=userInput.value;
+    const div1 =document.createElement('div');
+    div1.innerHTML=`<i class="fa-solid fa-xmark"></i>`;
+    div1.setAttribute('onclick','removeitem(event)')
+   
+    li.append(div,div1);
+    foodContaiener.append(li);
+   }
+}
+
+btn.addEventListener('click',handleInuptFood);
+
+userInput.addEventListener('keyup',(event)=>{
+   if(event.key === "Enter"){
+      handleInuptFood();
+   }else if(event.key === "KeyZ" && (event.ctrlkey="true"||event.metakey))
+   {
+       userInput.value='';
+   }
 });
+                                                                        
+//remove() 
+function removeitem(event){
+  const removeLi =event.target.parentNode.parentNode;
+  removeLi.remove();  
+}
+
+
+
+
+
+
